5480381b5fbe3e7b6e36e675b67b17023fc3e008
Jinrei97 <121897304+Jinrei97@users.noreply.github.com>
2024-07-08 20:34:07 +0200

    Generazione html spostata su file js.

--- src/loadPage.js
+++ src/loadPage.js
@@ -7,22 +7,22 @@ function createImg(clas, url, alt) {
 }
 
 export default function loadPage(left, center, right) {
-    const content = document.querySelector(".content");
+    const content = document.querySelector("#content");
 
     const img_div = document.createElement("div");
-    const img_text = Document.createElement("div");
+    const img_text = document.createElement("div");
     const h1 = document.createElement("h1");
-    const p = Document.createElement("p");
+    const p = document.createElement("p");
     img_div.classList.add("img");
     img_text.classList.add("img_text");
     h1.textContent = "Ristorante/Pizzeria - Da Mario";
     p.textContent = "Amore per il dettagli , ospitalità e piatti freschi e gustosi sono ciò che ci contraddistingue da sempre";
 
-    const body_div = document.querySelector("div");
+    const body_div = document.createElement("div");
     const img_left = createImg("left", left, "roba non meglio identificabile");
     const img_center = createImg("center", center, "spaghetti");
     const img_right = createImg("right", right, "pomodori");
-    body_div.classList.add(body);
+    body_div.classList.add("body");
 
     img_text.appendChild(h1);
     img_text.appendChild(p);
